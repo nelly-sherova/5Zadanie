98c0788724282cfaa89967c4e07f5b5b68ca7eba
unknown <lvova.nikol@mail.ru>
2020-03-22 14:29:05 +0300

    created console .net core project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("N = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Количество полных минут : {n/60}");
+            Console.WriteLine($"Количество полных часов : {n/3600}");
+        }
+    }
+}
